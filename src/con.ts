88a257f2c44bd683def3a8f779e5599ce50bbef6
Sora Suegami <suegamisora@gmail.com>
2019-02-15 13:38:46 +0900

    change def pos diff

--- src/con.ts
+++ src/con.ts
@@ -24,7 +24,7 @@ export const constant = {
     cycle:126144000,
     gas_limit:25,
     pow_target:Math.pow(2,256),
-    def_pos_diff:0.5,
+    def_pos_diff:0.1,
     lwma_size:8,
     unit_rate:0.99,
     compatible_version:compatible_version
