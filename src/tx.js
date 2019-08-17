691a649fd974754a614cd93cda351ab96991757f
Sora Suegami <suegamisora@gmail.com>
2019-08-17 12:25:34 +0900

    fix accept_ref_tx

--- src/tx.js
+++ src/tx.js
@@ -433,7 +433,17 @@ exports.accept_ref_tx = async (ref_tx, output_states, height, block_hash, index,
         res[s.owner] = s.data[2] || "00";
         return res;
     }, {});
-    const changed = await contract_1.default.basic.ref_tx_change(bases, output_states, requester, refresher, fee, gas, height, income_map);
+    const failed_state = (states, income_map, native_states) => {
+        const requester_state = states.filter(s => big_integer_1.default(s.token, 16).eq(big_integer_1.default(constant_1.constant.native, 16)) && s.owner === requester)[0];
+        const refresher_state = states.filter(s => big_integer_1.default(s.token, 16).eq(big_integer_1.default(constant_1.constant.native, 16)) && s.owner === refresher)[0];
+        const validator_keys = Object.entries(income_map).filter(obj => obj[1] != '00').map(obj => obj[0]);
+        const validator_states = native_states.filter(s => validator_keys.indexOf(s.owner) != -1);
+        const sum = [requester_state, refresher_state].concat(validator_states).filter(s => s != null);
+        const keys = sum.map(s => s.owner);
+        return sum.filter((s, i) => keys.indexOf(s.owner) === i);
+    };
+    const pre_ref_states = ref_tx.meta.refresh.success === 1 ? output_states : failed_state(output_states, income_map, native_base_states);
+    const changed = await contract_1.default.basic.ref_tx_change(bases, pre_ref_states, requester, refresher, fee, gas, height, income_map);
     const lock_states = await P.map(bases, async (key) => {
         return await data.read_from_trie(trie, lock_db, key, 1, lock_set.CreateLock(key));
     });
